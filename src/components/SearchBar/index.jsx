80ffe9f21a8b105c456ba26b2776901166a3f7e4
Jovanka Samudra <samudrajovanka20@gmail.com>
2022-04-01 18:02:35 +0700

    feat: search functionalities

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
+import config from '../../lib/config';
 
-export default function SearchBar() {
+export default function SearchBar({ onSuccess }) {
+  const [text, setText] = useState('');
+
+  const handleInput = (e) => {
+    setText(e.target.value);
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const LIMIT = 12;
+    const GIPHY_KEY = process.env.REACT_APP_GIPHY_KEY;
+
+    const gifs = await fetch(
+        `${config.GIPHY_BASE_URL}/gifs/search?q=${text}&api_key=${GIPHY_KEY}&limit=${LIMIT}`
+      ).then((response) => response.json());
+
+    onSuccess(gifs.data);
+  }
+
   return (
-    <form className="form-search">
-      <input type="text" placeholder="Search..." className="form-search__input" required />
+    <form className="form-search" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Search..."
+        className="form-search__input"
+        required
+        onChange={handleInput}
+      />
       <button type="submit" className="form-search__button">Search</button>
     </form>
   )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,80 +1,29 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Gif from '../../components/Gif'
-// import SearchBar from '../../components/SearchBar'
-// import gifs from '../../data/gifs'
-import config from '../../lib/config'
+import SearchBar from '../../components/SearchBar';
 
-export default class Home extends Component {
-  state = {
-    gifs: [],
-    text: '',
+export default function Home() {
+  const [gifs, setGifs] = useState([]);
+
+  const handleSuccessSearch = (searchGifts) => {
+    setGifs(searchGifts);
   }
 
-  handleInput = (e) => {
-    this.setState({ text: e.target.value });
-  }
+  return (
+    <div>
+      <SearchBar onSuccess={handleSuccessSearch} />
 
-  getGifs = async (e) => {
-    e.preventDefault()
-
-    const { text: query } = this.state
-    const LIMIT = 12
-    const GIPHY_KEY = process.env.REACT_APP_GIPHY_KEY
-
-    const gifs = await fetch(
-        `${config.GIPHY_BASE_URL}/gifs/search?q=${query}&api_key=${GIPHY_KEY}&limit=${LIMIT}`
-      ).then((response) => response.json());
-
-    this.setState({ gifs: gifs.data });
-  };
-
-  render() {
-    return (
-      <div>
-        <form className="form-search" onSubmit={this.getGifs}>
-          <input
-            type="text"
-            placeholder="Search..."
-            className="form-search__input"
-            required
-            onChange={this.handleInput}
-          />
-          <button type="submit" className="form-search__button">Search</button>
-        </form>
-
-        <div className="gifs">
-          {this.state.gifs.map((gif) => (
-            gif.rating === 'g' && (
-              <Gif
-                key={gif.id}
-                url={gif.images.original.url}
-                title={gif.title}
-              />
-            )
-          ))}
-        </div>
+      <div className="gifs">
+        {gifs.map((gif) => (
+          gif.rating === 'g' && (
+            <Gif
+              key={gif.id}
+              url={gif.images.original.url}
+              title={gif.title}
+            />
+          )
+        ))}
       </div>
-    )
-  }
+    </div>
+  )
 }
-
-// export default function Home() {
-//   return (
-//     <div>
-//       <SearchBar />
-
-//       <div className="gifs">
-//         {gifs.map((gif) => (
-//           <React.Fragment key={gif.id}>
-//             {gif.rating === 'g' && (
-//               <Gif
-//                 url={gif.url}
-//                 title={gif.title}
-//               />
-//             )}
-//           </React.Fragment>
-//         ))}
-//       </div>
-//     </div>
-//   )
-// }
